86a308cb407fe690ae722c74e244ec4570375e12
JuliaPustovaya <pustovayajulia@gmail.com>
2018-07-09 17:26:57 +0300

    initial scripts

--- src/main/java/RunSqlScript.java
+++ src/main/java/RunSqlScript.java
@@ -6,6 +6,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+//https://www.mkyong.com/jdbc/how-to-run-a-mysql-script-using-java/
+
 import com.ibatis.common.jdbc.ScriptRunner;
 
 /**
@@ -16,18 +18,20 @@ public class RunSqlScript {
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		String aSQLScriptFilePath = "db/02-insert.sql";
+		String aSQLScriptFilePath = "/home/ypustova/Desktop/bookStore/src/main/resources/db/02-insert.sql";
 		// Create MySql Connection
 		Class.forName("com.mysql.jdbc.Driver");
 		Connection con = DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306", "root", "root");
+				"jdbc:mysql://localhost:3306/BOOK_STORE", "root", "root");
+
+
+
 		Statement stmt = null;
 		try {
 			// Initialize object for ScripRunner
 			ScriptRunner sr = new ScriptRunner(con, false, false);
 			// Give the input file to Reader
-			Reader reader = new BufferedReader(
-					new FileReader(aSQLScriptFilePath));
+			Reader reader = new BufferedReader(new FileReader(aSQLScriptFilePath));
 			// Exctute script
 			sr.runScript(reader);
 		}
--- src/main/resources/db/02-insert.sql
+++ src/main/resources/db/02-insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO author(firstName, lastName, sex, birth, death) VALUES ('F','B','m','1965-06-25','1965-06-25')
+INSERT INTO author(firstName, lastName, sex, birth, death) VALUES ('F','B','m','1965-06-25','1965-06-25');
